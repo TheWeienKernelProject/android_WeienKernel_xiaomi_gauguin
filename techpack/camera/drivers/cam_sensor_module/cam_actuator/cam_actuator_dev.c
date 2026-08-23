b5db6641825e61540c7db784a190c479e1e4c9f1
普通的玩家863 <liuweien339@gmail.com>
2026-08-23 16:23:07 +0800

    Update cam_actuator_dev.c

--- techpack/camera/drivers/cam_sensor_module/cam_actuator/cam_actuator_dev.c
+++ techpack/camera/drivers/cam_sensor_module/cam_actuator/cam_actuator_dev.c
@@ -4,10 +4,10 @@
  */
 
 #include "cam_actuator_dev.h"
-#include "cam_req_mgr_dev.h"
+#include "../../cam_req_mgr/cam_req_mgr_dev.h"
 #include "cam_actuator_soc.h"
 #include "cam_actuator_core.h"
-#include "cam_trace.h"
+#include "../../cam_util/scam_trace.h"
 
 static long cam_actuator_subdev_ioctl(struct v4l2_subdev *sd,
 	unsigned int cmd, void *arg)
