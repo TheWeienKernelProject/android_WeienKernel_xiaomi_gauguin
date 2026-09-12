9641fbdf386d9fd87b0d89921456da7759130976
普通的玩家863 <liuweien339@gmail.com>
2026-09-12 18:03:22 +0800

    Update super.c

--- fs/super.c
+++ fs/super.c
@@ -449,8 +449,6 @@ void generic_shutdown_super(struct super_block *sb)
 		cgroup_writeback_umount();
 
 		evict_inodes(sb);
-        /* only nonzero refcount inodes can have marks */
-        fsnotify_sb_delete(sb);
         security_sb_delete(sb);
 
 		if (sb->s_dio_done_wq) {
